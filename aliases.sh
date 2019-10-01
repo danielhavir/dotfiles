c876c53f836ebd0bd93028f284ee34a1468b4a4f
Daniel Havir <havir.daniel@gmail.com>
2019-10-01 00:38:12 -0700

    update

--- aliases.sh
+++ aliases.sh
@@ -11,4 +11,14 @@ alias pingg="ping google.com -c 10"
 # Github
 alias gits="git status"
 alias gadd="git add"
+alias gpo="git push origin"
+
+# Google Cloud
+alias gssh="gcloud compute ssh"
+alias glist="gcloud compute instances list"
+alias gstart="gcloud compute instances start"
+alias gstop="gcloud compute instances stop"
+
+# nVidia
+alias smi="nvidia-smi -l 1"
 
