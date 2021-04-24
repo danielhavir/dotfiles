689b09d5dc3bf6fe24f45233e616ad5dd40cae29
Daniel Havir <havir.daniel@gmail.com>
2021-04-24 01:13:27 -0700

    update shell scripts

--- setup.sh
+++ setup.sh
@@ -2,15 +2,66 @@
 
 set -e
 
+SKIP_FISH=0
+WITH_K8S=0
+LINK_VIMRC=0
+WITH_GO=0
+WITH_NODE=0
+
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+
+  case $key in
+    --skip-fish|--skip_fish)
+      SKIP_FISH=1
+      shift
+      ;;
+    --with-k8s|--with_k8s)
+      WITH_K8S=1
+      shift
+      ;;
+    --with-go|--with_go)
+      WITH_GO=1
+      shift
+      ;;
+    --with-node|--with_node)
+      WITH_NODE=1
+      shift
+      ;;
+    --link-vimrc|--link_vimrc)
+      LINK_VIMRC=1
+      shift
+      ;;
+    *)
+      tput setaf 3
+      echo "Unknown option $1"
+      tput sgr0
+      shift
+      ;;
+esac
+done
+
 os=$(uname)
 
 if [ "$os" == "Linux" ]
 then
   profilefile="$HOME/.bashrc"
   echo "Known OS: $os => using $profilefile"
-  ./utils/install_fish.sh || echo "could not install fish, skipping."
+  if [ $SKIP_FISH = 0 ]
+  then
+    ./utils/install_fish.sh || echo "could not install fish, skipping."
+  fi
   ./utils/install_vim.sh || echo "could not install vim, skipping."
-  ./utils/install_node.sh || echo "could not install nodejs (for coc), skipping."
+  if [ $WITH_GO = 1 ]
+  then
+    ./utils/install_go.sh
+  fi
+  if [ $WITH_NODE = 1 ]
+  then
+    ./utils/install_node.sh || echo "could not install nodejs (for coc), skipping."
+  fi
+  sudo apt-get install -y silversearcher-ag || echo "could not install Ag, skipping."
 elif [ "$os" == "Darwin" ]
 then
   profilefile="$HOME/.bash_profile"
@@ -44,9 +95,16 @@ echo -e "$(cat bash_profile)\n\n$(cat "$profilefile")" > "$profilefile"
   echo
   cat aliases.sh
   echo
-  cat kubernetes-helpers.sh
-  echo
 } >> "$profilefile"
+
+if [ $WITH_K8S = 1 ]
+then
+  echo "Adding K8s helpers to $profilefile"
+  {
+    cat kubernetes-helpers.sh
+    echo
+  } >> "$profilefile"
+fi
 
 # Copy SSH configuration
 mkdir -p "$HOME/.ssh/"
@@ -61,15 +119,23 @@ echo "vim-plug set up, run \":PlugInstall\" in Vim"
 tput sgr0
 
 # Setup .vimrc
-./utils/install_vimrc.sh
+if [ $LINK_VIMRC = 1 ]
+then
+  ./utils/install_vimrc.sh --link
+else
+  ./utils/install_vimrc.sh
+fi
 
-# Copy configuration for Fish shell
-fish_home="$HOME/.config/fish"
-mkdir -p "$fish_home"
-cp config/fish/* "$fish_home"
+if [ $SKIP_FISH = 0 ]
+then
+  # Copy configuration for Fish shell
+  fish_home="$HOME/.config/fish"
+  mkdir -p "$fish_home"
+  cp config/fish/* "$fish_home"
 
-# Install Fisher
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+  # Install Fisher
+  curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+fi
 
 # Copy JSON IO helpers
 cp jsonio.py "$HOME"
